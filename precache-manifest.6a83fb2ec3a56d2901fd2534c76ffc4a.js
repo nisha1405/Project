4b59684e9884a9a679951dce46eff184f45e1c67
Nisha <nishashanisingh14@gmail>
2020-07-25 15:16:05 +0530

    Updates

---
+++ precache-manifest.6a83fb2ec3a56d2901fd2534c76ffc4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bada60f1a00706336124621e0a6092",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "99f5cd1f85b50a8fcfee",
+    "url": "/project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/project/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f5cd1f85b50a8fcfee",
+    "url": "/project/static/js/main.a9a36f8e.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project/static/media/logo.5d5d9eef.svg"
+  }
+]);
